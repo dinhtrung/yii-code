d9ce334b443c3f6db5ad95c564f53980e25c4e84
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-03-27 15:28:34 +0700

    Remove opencode module, master only contain 4 essentials module

--- protected/modules/opencode/views/layouts/_menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-$this->mainMenu['opencode'] = array(
-	'label' => Yii::t('opencode', 'opencode'),
-	'url'	=>	array('/opencode'),
-	'visible'	=>	Yii::app()->user->checkAccess('Opencode.Default.Index'),
-);
