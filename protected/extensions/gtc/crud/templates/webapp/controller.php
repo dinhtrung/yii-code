8317aae778f7e522d1f4f6910536d3d3ffc271fc
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-04-04 11:04:17 +0700

    - Gii generator for WebBaseController

---
+++ protected/extensions/gtc/crud/templates/webapp/controller.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * This is the template for generating a controller class file for CRUD feature.
+ * The following variables are available in this template:
+ * - $this: the CrudCode object
+ */
+?>
+<?php echo "<?php\n"; ?>
+
+class <?php echo $this->controllerClass; ?> extends WebBaseController
+{
+
+	public function allowedActions(){
+		return '';
+	}
+
+	/**
+	 * Available actions
+	 */
+	public function actions(){
+		return array(
+			'index' => 'ext.actions.BrowseAction',
+			'view' 	=> 'ext.actions.ViewAction',
+			'create' => 'ext.actions.CreateAction',
+			'update' => 'ext.actions.UpdateAction',
+			'delete' => 'ext.actions.DeleteAction',
+			'settings' => 'ext.actions.SettingsAction',
+			'duplicate' => 'ext.actions.DuplicateAction',
+		);
+	}
+
+	/**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionView($id)
+	{
+		$this->render('view',array(
+			'model'=>$this->loadModel($id),
+		));
+	}
+
+	/**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreate()
+	{
+		$model=new <?php echo $this->modelClass; ?>;
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model, '<?php echo strtolower($this->modelClass); ?>-form');
+
+		if(isset($_POST['<?php echo $this->modelClass; ?>']))
+		{
+			$model->attributes=$_POST['<?php echo $this->modelClass; ?>'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>));
+		}
+
+		$this->render('create',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionUpdate($id)
+	{
+		$model=$this->loadModel($id);
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model, '<?php echo strtolower($this->modelClass); ?>-form');
+
+		if(isset($_POST['<?php echo $this->modelClass; ?>']))
+		{
+			$model->attributes=$_POST['<?php echo $this->modelClass; ?>'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>));
+		}
+
+		$this->render('update',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Deletes a particular model.
+	 * If deletion is successful, the browser will be redirected to the 'admin' page.
+	 * @param integer $id the ID of the model to be deleted
+	 */
+	public function actionDelete($id)
+	{
+		$this->loadModel($id)->delete();
+
+		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+		if(!isset($_GET['ajax']))
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+	}
+
+	/**
+	 * Lists all models.
+	 */
+	public function actionIndex()
+	{
+		$dataProvider=new CActiveDataProvider('<?php echo $this->modelClass; ?>');
+		$this->render('index',array(
+			'dataProvider'=>$dataProvider,
+		));
+	}
+
+	/**
+	 * Manages all models.
+	 */
+	public function actionAdmin()
+	{
+		$model=new <?php echo $this->modelClass; ?>('search');
+		$model->unsetAttributes();  // clear any default values
+		if(isset($_GET['<?php echo $this->modelClass; ?>']))
+			$model->attributes=$_GET['<?php echo $this->modelClass; ?>'];
+
+		$this->render('admin',array(
+			'model'=>$model,
+		));
+	}
+}
