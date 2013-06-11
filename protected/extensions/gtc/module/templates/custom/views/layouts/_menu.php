c38ab60eb226eac0f9b13f8e70c21d5b2dd1889a
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-06-11 09:46:19 +0700

    - Fix module typo

--- protected/extensions/gtc/module/templates/custom/views/layouts/_menu.php
+++ protected/extensions/gtc/module/templates/custom/views/layouts/_menu.php
@@ -1,13 +1,13 @@
 <?php echo "<?php\n"; ?>
 $this->mainMenu['<?php echo $this->moduleID; ?>'] = array(
-	'label' => Yii::t('<?php echo $this->moduleID; ?>', '<?php echo $this->moduleClass; ?>'),
+	'label' => Yii::t('<?php echo $this->moduleID; ?>', '<?php echo ucfirst($this->moduleID); ?>'),
 	'url'=>array('/<?php echo $this->moduleID; ?>/default/index'),
-	'visible' => Yii::app()->user->checkAccess('<?php echo $this->moduleClass; ?>.Default.Index'),
+	'visible' => Yii::app()->user->checkAccess('<?php echo ucfirst($this->moduleID); ?>.Default.Index'),
 	'items'=>array(
 		array(
-				'label' => Yii::t('?php echo $this->moduleID; ?>', 'Controller.Action'),
+				'label' => Yii::t('<?php echo $this->moduleID; ?>', 'Controller.Action'),
 				'url'=>array('/<?php echo $this->moduleID; ?>/controller/action'),
-				'visible' => Yii::app()->user->checkAccess('<?php echo $this->moduleClass; ?>.Controller.Action')
+				'visible' => Yii::app()->user->checkAccess('<?php echo ucfirst($this->moduleID); ?>.Controller.Action')
 			),
 	),
 );
