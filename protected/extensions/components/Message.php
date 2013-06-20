0e5d8ba106ced0d5c4ceca3500bba0b8b3841e8a
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-06-20 09:16:23 +0700

    - Add EMessage Source

---
+++ protected/extensions/components/Message.php
@@ -0,0 +1,57 @@
+<?php
+class Message extends BaseActiveRecord{
+
+    public $message,$category;
+
+    public function connectionId(){
+    	return Yii::app()->hasComponent('translateDb')?'translateDb':'db';
+    }
+
+	static function model($className=__CLASS__){
+		return parent::model($className);
+	}
+
+	function tableName(){
+		return '{{message}}';
+	}
+
+	function relations(){
+		return array(
+            'source'=>array(self::BELONGS_TO,'MessageSource','id'),
+		);
+	}
+
+
+	/*
+	 * CREATE TABLE Message
+		(
+		    id INTEGER,
+		    language VARCHAR(16),
+		    translation TEXT,
+		    PRIMARY KEY (id, language),
+		    CONSTRAINT FK_Message_SourceMessage FOREIGN KEY (id)
+		         REFERENCES SourceMessage (id) ON DELETE CASCADE ON UPDATE RESTRICT
+		);
+	 */
+	protected function createTable(){
+		$columns = array(
+				'id'	=>	'int',
+				'language'	=>	'string',
+				'translation'	=>	'text',
+		);
+		try {
+			$this->getDbConnection()->createCommand(
+				Yii::app()->getDb()->getSchema()->createTable($this->tableName(), $columns)
+			)->execute();
+			$this->getDbConnection()->createCommand(
+				Yii::app()->getDb()->getSchema()->addPrimaryKey('id_lang', $this->tableName(), 'id,language')
+			)->execute();
+			$ref = new MessageSource();
+			$this->getDbConnection()->createCommand(
+				Yii::app()->getDb()->getSchema()->addForeignKey('fk_message_sourcemessage', $this->tableName(), 'id', $ref->tableName(), 'id')
+			)->execute();
+		} catch (CDbException $e){
+			Yii::log($e->getMessage(), 'warning');
+		}
+	}
+}
---
+++ protected/extensions/components/MessageSource.php
@@ -0,0 +1,47 @@
+<?php
+class MessageSource extends BaseActiveRecord{
+
+    public $language;
+
+	static function model($className=__CLASS__){
+		return parent::model($className);
+	}
+
+	public function connectionId(){
+		return Yii::app()->hasComponent('translateDb')?'translateDb':'db';
+	}
+
+	function tableName(){
+		return '{{sourcemessage}}';
+	}
+
+	/*
+	 * CREATE TABLE SourceMessage
+	(
+			id INTEGER PRIMARY KEY,
+			category VARCHAR(32),
+			message TEXT
+	);
+	*/
+	protected function createTable(){
+		$columns = array(
+				'id'	=>	'pk',
+				'category'	=>	'string',
+				'message'	=>	'text',
+		);
+		try {
+			$this->getDbConnection()->createCommand(
+					Yii::app()->getDb()->getSchema()->createTable($this->tableName(), $columns)
+			)->execute();
+		} catch (CDbException $e){
+			Yii::log($e->getMessage(), 'warning');
+		}
+	}
+
+
+	function relations(){
+		return array(
+				'mt'=>array(self::HAS_MANY,'Message','id','joinType'=>'inner join'),
+		);
+	}
+}
