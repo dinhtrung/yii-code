56142d204565e89a6a77be892ab8a7f4eba085c0
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-06-20 13:54:46 +0700

    - Have to fix alot of typo?

---
+++ protected/extensions/gtc/yiistrap/templates/baseAR/controller.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * This is the template for generating a controller class file for CRUD feature.
+ * The following variables are available in this template:
+ * - $this: the BootstrapCode object
+ */
+?>
+<?php echo "<?php\n"; ?>
+
+class <?php echo $this->controllerClass; ?> extends WebBaseController
+{
+	/**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionView()
+	{
+		$this->render('view<?php echo $this->modelClass; ?>',array(
+			'model'=>$this->loadModel('<?php echo $this->modelClass; ?>'),
+		));
+	}
+
+	/**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreate()
+	{
+		$model=new <?php echo $this->modelClass; ?>;
+
+		$this->performAjaxValidation($model, '<?php echo $this->class2id($this->modelClass); ?>-form');
+
+		if(isset($_POST['<?php echo $this->modelClass; ?>']))
+		{
+			$model->attributes=$_POST['<?php echo $this->modelClass; ?>'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>));
+		}
+
+		$this->render('create<?php echo $this->modelClass; ?>',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionUpdate()
+	{
+		$model=$this->loadModel('<?php echo $this->modelClass; ?>');
+
+		$this->performAjaxValidation($model, '<?php echo $this->class2id($this->modelClass); ?>-form');
+
+		if(isset($_POST['<?php echo $this->modelClass; ?>']))
+		{
+			$model->attributes=$_POST['<?php echo $this->modelClass; ?>'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>));
+		}
+
+		$this->render('update<?php echo $this->modelClass; ?>',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Deletes a particular model.
+	 * If deletion is successful, the browser will be redirected to the 'admin' page.
+	 * @param integer $id the ID of the model to be deleted
+	 */
+	public function actionDelete()
+	{
+		if (Yii::app()->request->isPostRequest)
+		{
+			// we only allow deletion via POST request
+			$this->loadModel(<?php echo $this->modelClass; ?>)->delete();
+
+			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+			if(!isset($_GET['ajax']))
+				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+		}
+		else
+			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
+	}
+
+	/**
+	 * Lists all models.
+	 */
+	public function actionIndex()
+	{
+		$model=new <?php echo $this->modelClass; ?>('search');
+		$this->render('index<?php echo $this->modelClass; ?>',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Manages all models.
+	 */
+	public function actionAdmin()
+	{
+		$model=new <?php echo $this->modelClass; ?>('search');
+		$model->unsetAttributes();  // clear any default values
+		if(isset($_GET['<?php echo $this->modelClass; ?>']))
+			$model->attributes=$_GET['<?php echo $this->modelClass; ?>'];
+
+		$this->render('admin<?php echo $this->modelClass; ?>',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer $id the ID of the model to be loaded
+	 * @return <?php echo $this->modelClass; ?> the loaded model
+	 * @throws CHttpException
+	 */
+	public function loadModel($model = FALSE)
+	{
+		if(!$model)
+			$model = str_replace('Controller', '', get_class($this));
+
+		if($this->_model === null) {
+			if(isset($_GET['id'])){
+				$this->_model = CActiveRecord::model($model)->findbyPk(urldecode($_GET['id']));
+			} else {
+				$pkeys = CActiveRecord::model($model)->getTableSchema()->primaryKey;
+				if (is_string($pkeys) && isset($_GET[$pkeys])){
+					$this->_model = CActiveRecord::model($model)->findbyPk(urldecode($_GET[$pkeys]));
+				} elseif (is_array($pkeys)){
+					$tmp = array_flip($pkeys);
+					foreach ($pkeys as $field) {
+						if (isset($_GET[$field])) $tmp[$field] = urldecode($_GET[$field]);
+						else { $tmp = FALSE; break; }
+					}
+					if (! empty($tmp))
+					$this->_model = CActiveRecord::model($model)->findbyPk($tmp);
+				}
+			}
+			if($this->_model===null)
+				throw new CHttpException(404, Yii::t('app', 'The requested page does not exist.'));
+		}
+		return $this->_model;
+	}
+
+	/**
+	 * Performs the AJAX validation.
+	 * @param <?php echo $this->modelClass; ?> $model the model to be validated
+	 */
+	protected function performAjaxValidation($model)
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='<?php echo $this->class2id($this->modelClass); ?>-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+}
